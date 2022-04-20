d3428dd10a8475196dd1d0c44c50e816a56d573c
LansfordC <69258564+LansfordC@users.noreply.github.com>
2022-04-20 09:19:15 -0400

    asdf

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
+double add(double num1, double num2);
+
 int main() {
     std::cout << "Hello World!" << std::endl;
+    std::cout << add(5,4) << std::endl;
     return 0;
+}
+
+double add(double num1, double num2) {
+    return num1 + num2;
 }
